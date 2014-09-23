ca4c17c83ba8deff70f1aed75eb212d72db3f2c4
jerem <x>
2014-09-23 16:01:55 +0200

    vehicule

--- jpasample/src/main/java/domain/Vehicule.java
+++ jpasample/src/main/java/domain/Vehicule.java
@@ -2,4 +2,15 @@ package domain;
 
 public class Vehicule {
 
+	
+private int id;
+private String model;
+private String marque;
+private String immatriculation;
+private int nbplaces;
+private String couleur;
+private int confort;
+private boolean bagage;
+
+
 }
