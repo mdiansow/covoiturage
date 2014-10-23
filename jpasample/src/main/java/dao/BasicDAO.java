dad026544dd156b547847c032f11223dd695152a
mdsow <mdiansow@yahoo.fr>
2014-10-23 08:17:31 +0200

    Gestion du modèle de l'appli

--- jpasample/src/main/java/dao/BasicDAO.java
+++ jpasample/src/main/java/dao/BasicDAO.java
@@ -11,8 +11,10 @@ import java.util.Map;
 /**
  * Created by root on 19/10/14.
  */
-public class PersonneDAO implements IDao {
+public class BasicDAO<T> implements IDao {
     private EntityManager em;
+
+    private Class<T> entity;
     @Override
     public void setEm(EntityManager em) {
         this.em = em;
@@ -34,11 +36,14 @@ public class PersonneDAO implements IDao {
 
     @Override
     public List findAll() {
+        em.getTransaction().begin();
         List<Personne> resultList = em.createQuery("select a from m2gla_personne a",
                 Personne.class).getResultList();
+        em.getTransaction().commit();
+        em.close();
         System.out.println("Nb of persons: " + resultList.size());
         for (Personne next : resultList) {
-            System.out.println("next person: " + next);
+            System.out.println("next person: " + next.getNom());
         }
         return resultList;
     }
@@ -48,12 +53,15 @@ public class PersonneDAO implements IDao {
         System.out.println("Create de AbstractDAO : "+entity.getClass().toString());
         if(entity == null)
             throw new PersistenceException("Entity to persist may not be null");//throw Persistence exception
+        em.getTransaction().begin();
         try {
             em.persist(entity);
         }
         catch(Exception e){
             System.out.println("error persist commande"+e.toString());
         }
+        em.getTransaction().commit();
+        em.close();
     }
 
     @Override
