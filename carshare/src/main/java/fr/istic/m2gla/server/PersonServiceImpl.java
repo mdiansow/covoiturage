0be0b188d7c0857a3b29c45df014a5cd69e92886
mdsow <mdiansow@yahoo.fr>
2014-11-13 08:52:17 +0100

    Le projet avec la nouvelle config

---
+++ carshare/src/main/java/fr/istic/m2gla/server/PersonServiceImpl.java
@@ -0,0 +1,74 @@
+package fr.istic.m2gla.server;
+
+import fr.istic.m2gla.shared.Personne;
+
+import javax.persistence.*;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Collection;
+
+/**
+ * Created by mds on 10/11/14.
+ */
+@Path("/user")
+public class PersonServiceImpl implements IPersonService{
+    private EntityManager em;
+
+    public PersonServiceImpl() {
+        EntityManagerFactory factory = Persistence
+                .createEntityManagerFactory("mysql");
+        em = factory.createEntityManager();
+        EntityTransaction t = em.getTransaction();
+        t.begin();
+        Personne personne = new Personne();
+        personne.setNom("SOW");
+        personne.setPrenom("Mamadou");
+        em.persist(personne);
+        t.commit();
+    }
+
+    @Override
+    @GET
+    @Path("search/{id}")
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Personne findById(@PathParam("id") long id) {
+        Personne result = null;
+        if (id < 0 || id < 1)
+            throw new PersistenceException("Id may not be null or negative");
+        result = em.find(Personne.class,id);
+        return result;
+    }
+
+    @Override
+    @GET
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Collection<Personne> findAll() {
+        return em.createQuery("select e from m2gla_personne as e").getResultList();
+    }
+
+    @Override
+    @POST
+    @Path("create")
+    public void create(Personne entity) {
+        System.out.println("Create de AbstractDAO : "+entity.getClass().toString());
+        if(entity == null)
+            throw new PersistenceException("Entity to persist may not be null");//throw Persistence exception
+        em.getTransaction().begin();
+        try {
+            em.persist(entity);
+        }
+        catch(Exception e){
+            System.out.println("error persist commande"+e.toString());
+        }
+    }
+
+    @Override
+    public void delete(Personne entity) {
+
+    }
+
+    @Override
+    public Personne update(Personne entity) {
+        return null;
+    }
+}
---
+++ carshare/src/main/java/fr/istic/m2gla/shared/Ville.java
@@ -0,0 +1,79 @@
+package fr.istic.m2gla.shared;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+@Entity(name = "m2gla_ville")
+public class Ville
+
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private long id;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private String nom;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	private String cp;
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Ville(){
+		super();
+	}
+
+    @Id
+    @GeneratedValue
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCp() {
+        return cp;
+    }
+
+    public void setCp(String cp) {
+        this.cp = cp;
+    }
+}
+
