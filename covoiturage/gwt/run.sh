0fb8bdf08d5dc0f5bdec1117a9488ec8794ebfd2
mdsow <mdiansow@yahoo.fr>
2014-11-21 18:01:22 +0100

    Renommage

--- covoiturage/gwt/run.sh
+++
@@ -1,1 +0,0 @@
-mvn compile exec:java -Dexec.mainClass=fr.istic.m2gla.server.JpaTest
--- jpasample/src/main/java/services/PersonServicesImpl.java
+++
@@ -1,7 +0,0 @@
-package services;
-
-/**
- * Created by root on 23/10/14.
- */
-public class PersonServicesImpl {
-}
